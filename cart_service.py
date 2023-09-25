cc92f4e82756e4eaf6471fdb720ce4690b00074e
asalkey82 <89543219+asalkey82@users.noreply.github.com>
2023-09-24 23:44:13 -0400

    Update cart_service.py

--- cart_service.py
+++ cart_service.py
@@ -17,7 +17,6 @@ customers = [
 
 BASE_URL = "https://carl-mart.onrender.com"
 
-#Endpoint 1: get user cart
 @app.route('/cart/<int:user_id>', methods=['GET'])
 def get_cart(user_id):
     customer = next((customer for customer in customers if customer["user"] == user_id), None)
@@ -26,8 +25,6 @@ def get_cart(user_id):
     else:
         return jsonify({"error": "Customer not found"}), 404
 
-#Endpoint 2: Add product to user cart. 
-#Makes another call to product server to remove product from store
 @app.route('/cart/<int:user_id>/add/<int:product_id>', methods=['POST'])
 def addToCart(product_id, user_id):
     response = requests.get(f'{BASE_URL}/products/{product_id}')
@@ -39,8 +36,6 @@ def addToCart(product_id, user_id):
     requests.post(f'{BASE_URL}/remove/{product_id}')
     return jsonify({"message": "Product added"})
 
-#Endpoint 3: Remove product from user cart. 
-#Makes another call to product server to add product to store
 @app.route('/cart/<int:user_id>/remove/<int:product_id>', methods=['POST'])
 def removeFromCart(product_id, user_id):
     response = requests.get(f'{BASE_URL}/products/{product_id}')

fbb28755b49992e12e9d210ad6c8e1a751a7a881
asalkey82 <89543219+asalkey82@users.noreply.github.com>
2023-09-24 21:38:38 -0400

    Update cart_service.py

--- cart_service.py
+++ cart_service.py
@@ -15,9 +15,9 @@ customers = [
     }
 ]
 
-BASE_URL = "https://carl-mart-shopping-cart.onrender.com"
+BASE_URL = "https://carl-mart.onrender.com"
 
-@app.route(f'{BASE_URL}/cart/<string:user_id>', methods=['GET'])
+@app.route('/cart/<string:user_id>', methods=['GET'])
 def get_cart(user_id):
     customer = next((customer for customer in customers if customer["user"] == user_id), None)
     if customer:
@@ -25,26 +25,26 @@ def get_cart(user_id):
     else:
         return jsonify({"error": "Task not found"}), 404
     
-@app.route(f'{BASE_URL}/cart/add/<int:product_id>', methods=['POST'])
+@app.route('/cart/add/<int:product_id>', methods=['POST'])
 def addToCart(product_id):
-    response = requests.get(f'https://carl-mart.onrender.com/products/{product_id}')
+    response = requests.get(f'{BASE_URL}/products/{product_id}')
     data = response.json()
 
     customers[0]['cart'][product_id-1]['price'] += data["product"]["price"]
     customers[0]['cart'][product_id-1]['quantity'] += 1
 
-    requests.post(f'https://carl-mart.onrender.com/remove/{product_id}')
+    requests.post(f'{BASE_URL}/remove/{product_id}')
     return jsonify({"message": "Product added"})
 
-@app.route(f'{BASE_URL}/cart/remove/<int:product_id>', methods=['POST'])
+@app.route(f'/cart/remove/<int:product_id>', methods=['POST'])
 def removeFromCart(product_id):
-    response = requests.get(f'https://carl-mart.onrender.com/products/{product_id}')
+    response = requests.get(f'{BASE_URL}/products/{product_id}')
     data = response.json()
    
     customers[0]['cart'][product_id-1]['price'] -= data["product"]["price"]
     customers[0]['cart'][product_id-1]['quantity'] -= 1
 
-    requests.post(f'https://carl-mart.onrender.com/add/{product_id}')
+    requests.post(f'{BASE_URL}/add/{product_id}')
     return jsonify({"message": "Product removed"})
 
 if __name__ == '__main__':
